804fc82c3607681679010aa2183416e0034d7b6b
dcpneto <dcp_neto@live.com>
2018-04-03 13:28:07 -0300

    update for pt-br and personalize colorchat

--- chatclient.py
+++ chatclient.py
@@ -1,61 +1,67 @@
 #!/usr/bin/env python3
-""" Script for chat client using Tkinter -- Scritp para o chat usando TK """
+""" Script para o cliente de chat usando TKinter """
 
 from socket import *
 from threading import Thread
 import tkinter
 
 def receive():
-    """" Handles receive of messages -- Configurando o recebimento de menssagens """
+    """" Configurando o recebimento de menssagens """
     while True:
         try:
             msg = client_socket.recv(BUFFSIZE).decode("utf8")
             msg_list.insert(tkinter.END, msg)
-        except OSError:  #Possibly client has left the chat -- Quando der esse erro, possivelmente o cliente deslogou
+        except OSError:  # Quando der esse erro, possivelmente o cliente deslogou
             break
 
-def send(event = None): #Event is pass by binders -- evento sera passado por bind (botao)
-    """" Handles sendig of messages -- Configurando o envio de mensagens """
+def send(event = None): # Evento sera passado por bind (botao)
+    """" Configurando o envio de mensagens """
     msg = my_msg.get()
-    my_msg.set("")  #Clear input filed - limpar o campo de texto
+    my_msg.set("")  # Limpar o campo de texto
     client_socket.send(bytes(msg, "utf8"))
-    if msg == "{quit}":
+    if msg == "{sair}":
         client_socket.close()
         top.quit()
 
 def on_closing(event = None):
-    """This functions is called when the window is closed -- Essa função é chamada quando a janela é fechada"""
-    my_msg.set("{quit}")
+    """Essa função é chamada quando a janela é fechada"""
+    my_msg.set("{sair}")
     send()
 
 top = tkinter.Tk()
-top.title("Chat App - ADEY")
+top.title("IFChat V0.0.1a")
+
+top.configure(bg="Black")
 
 messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  #For can send messages -- Para poder enviar as mensagens
-my_msg.set("Type your message here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  #To navigate through past messages - Pra ver as mensagens anteriores
+my_msg = tkinter.StringVar()  # Para poder enviar as mensagens
+my_msg.set("Digite sua mensagem aqui.")
+scrollbar = tkinter.Scrollbar(messages_frame)  # Para ver as mensagens anteriores
+scrollbar.configure(bg="Black")
 
-#Following will contain the messages.  -- Abaixo armazenará as mensagens
+# Abaixo armazenará as mensagens
 
-msg_list = tkinter.Listbox(messages_frame, height = 15, width = 50, yscrollcommand = scrollbar.set)
+msg_list = tkinter.Listbox(messages_frame, height = 15, width = 60, yscrollcommand = scrollbar.set)
 scrollbar.pack(side = tkinter.RIGHT, fill = tkinter.Y)
 msg_list.pack(side = tkinter.LEFT, fill = tkinter.BOTH)
+msg_list.configure(bg="Black", fg="Green", highlightbackground="#0d0d0d")
 msg_list.pack()
 messages_frame.pack()
 
 entry_field = tkinter.Entry(top, textvariable = my_msg)
 entry_field.bind("<Return>", send)
+entry_field.configure(bg="Black", fg="Green", relief="ridge", highlightbackground="Black")
 entry_field.pack()
-send_button = tkinter.Button(top, text = "Send", command = send)
+send_button = tkinter.Button(top, text = "Enviar", command = send)
+send_button.configure(bg="Black", fg="Green", relief="raised", highlightbackground="Black")
 send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
-#---Now comes the socket part -- Vamos começar a parte dos sockets---
+#--- Vamos começar a parte dos sockets ---
 
-HOST = input('Enter host: ')
-PORT = input('Enter port: ')
+HOST = input('Host: ')
+PORT = input('Port: ')
 
 if not PORT:
     PORT = 33000
@@ -70,4 +76,4 @@ client_socket.connect(ADDR)
 
 receive_thread = Thread(target = receive)
 receive_thread.start()
-tkinter.mainloop()  #Starts GUI execution -- Iniciar a interface gráfica
+tkinter.mainloop()  # Iniciar a interface gráfica

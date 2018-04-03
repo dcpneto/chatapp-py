41aa457117f09242b4bcfd90d47d1023da34f547
dcpneto <dcp_neto@live.com>
2018-04-03 13:26:46 -0300

    Update for pt-br

--- chatserver.py
+++ chatserver.py
@@ -1,44 +1,44 @@
 #!/usr/bin/env python3
 
-""" Server multithread for chat app -- Servidor multitread para aplicação de chat"""
+"""Servidor multithread para aplicação de chat"""
 
 from socket import *
 from threading import Thread
 
 def accept_incoming_connections():
-    """ Setup conexions whit clients -- Definindo conexões com clientes """
+    """ Definindo conexões com clientes """
     
     while True:
         client, client_address = SERVER.accept()
-        print("%s: %s connected." % client_address)
-        client.send(bytes("Greetings from the batcave!" +
-                            "Now, type you nick and press enter", "utf8"))
+        print("%s: %s conectado." % client_address)
+        client.send(bytes("Bem-vindo ao ThreadChat V0.0.1a! " +
+                            "Insira seu apelido e pressione enter.", "utf8"))
         addresses[client] = client_address
         Thread(target = handle_client, args = (client,)).start()
 
-def handle_client(client):  # Takes client socket as argument -- pegando o socket do client como argumento
-    """ Handles a single client connection -- Configurando a conexão com um cliente"""
+def handle_client(client):  # Pegando o socket do client como argumento
+    """Configurando a conexão com um cliente"""
     name = client.recv(BUFFSIZE).decode("utf8")
-    welcome = 'Welcome %s! If you ever want quit, type {quit} to exit.' % name
+    welcome = 'Bem-vindo %s! Se deseja sair, digite {sair} para sair.' % name
     client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat" % name
+    msg = "%s entrou na conversa." % name
     broadcast(bytes(msg, "utf8"))
     clients[client] = name
 
     while True:
         msg = client.recv(BUFFSIZE)
-        if msg != bytes("{quit}", "utf8"):
+        if msg != bytes("{sair}", "utf8"):
             broadcast(msg, name + ": ")
         else:
-            client.send(bytes("{quit}", "utf8"))
+            client.send(bytes("{sair}", "utf8"))
             client.close()
             del clients[client]
-            broadcast(bytes("%s has left the chat."% name, "utf8"))
+            broadcast(bytes("%s Saiu da conversa."% name, "utf8"))
             break
 
-def broadcast (msg, prefix = ""):  #prefix is for name identification -- prefixo é para identificar quem fala
+def broadcast (msg, prefix = ""):  # Prefixo é para identificar quem fala
     
-    #""" Broadcasts a message to all the clients -- Enviar uma mensagem em broadcast para todos """"
+    #""" Enviar uma mensagem em broadcast para todos """"
     
     for sock in clients:
         sock.send(bytes(prefix, "utf8")+ msg)
@@ -56,10 +56,10 @@ SERVER.bind(ADDR)
 
 
 if __name__ == "__main__":
-    SERVER.listen(5)   #Listen for 5 conections at max -- permita no m[aximo 5 conexões
-    print("Waiting for connection...")
+    SERVER.listen(5)   # Permissão para no máximo 5 conexões simultâneas
+    print("Aguardando Conexão...")
     ACCEPT_THREAD = Thread(target = accept_incoming_connections)
-    ACCEPT_THREAD.start()  #Start the infinite loop -- iniciando o loop infinito
+    ACCEPT_THREAD.start()  # Iniciando o loop infinito
     ACCEPT_THREAD.join()
     SERVER.close()
     
